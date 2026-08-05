754955ccc18b9ee5bdd7828be438edaf0de97f0c
Martín Calderón <dev.msco@gmail.com>
2026-08-05 16:34:41 -0700

    feat: add sample products & product list display

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,20 @@
-import { Button } from "@/components/ui/button";
+import ProductList from "@/components/shared/product/product-list";
+import sampleData from "@/db/sample-data";
 
 export const metadata = {
   title: "Home",
 };
 
 const Homepage = () => {
-  return <></>;
+  return (
+    <>
+      <ProductList
+        data={sampleData.products}
+        title="Newest arrivals"
+        limit={4}
+      />
+    </>
+  );
 };
 
 export default Homepage;
